a9bf1fe150447ca74be540985a7767dd3633a779
Lizania Lolita Beltran Castro <lizaniabeltran2@icloud.com>
2026-06-07 13:19:24 -0700

    Corregir CORS para Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.use(helmet());
 app.use(cors({
     origin: [
         'http://localhost:5500',
-        'http://127.0.0.1:5500'
+        'http://127.0.0.1:5500',
         'https://scentvault-frontend.vercel.app'
     ]
 }));
